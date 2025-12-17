9a602d4a2008f9aadc72e117f7ec09f9514f299a
Axel DG <alexander18br17@gmail.com>
2025-12-16 22:29:17 -0500

    2Comit-16-12-2025

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailModule } from './mail/mail.module';
+import { ImagesModule } from './images/images.module';
 
 @Module({
-  imports: [MailModule],
+  imports: [MailModule, ImagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
