90668df111531e11680674c21888a2f0bdb82eca
Axel DG <alexander18br17@gmail.com>
2025-12-16 22:31:49 -0500

    4Comit-16-12-2025

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 nest g module requests
 nest g controller requests
 nest g service requests
+
+nest g module transactions
+nest g controller transactions
+nest g service transactions
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MailModule } from './mail/mail.module';
 import { ImagesModule } from './images/images.module';
 import { RequestsModule } from './requests/requests.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [MailModule, ImagesModule, RequestsModule],
+  imports: [MailModule, ImagesModule, RequestsModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
