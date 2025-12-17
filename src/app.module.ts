51fafed60ed7712f24299607ec57fcaa86dc827c
AlexMa77 <am9614843@gmail.com>
2025-12-16 22:20:41 -0500

    Creacion Modulo Locations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PropertiesModule } from './properties/properties.module';
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
-  imports: [PropertiesModule],
+  imports: [PropertiesModule, LocationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
