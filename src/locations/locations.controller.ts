707e5c4d1a70580f59f980b8bd311b17e1360cd7
AlexMa77 <am9614843@gmail.com>
2025-12-19 22:19:18 -0500

    Commit Locations

--- src/locations/locations.controller.ts
+++ src/locations/locations.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { LocationsService } from './locations.service';
+import { CreateLocationsDto } from './dto/create-locations.dto';
+import { UpdateLocationsDto } from './dto/update-locations.dto';
 
 @Controller('locations')
-export class LocationsController {}
+export class LocationsController {
+  constructor(private readonly locationsService: LocationsService) {}
+
+  @Post()
+  create(@Body() dto: CreateLocationsDto) {
+    return this.locationsService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.locationsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.locationsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() dto: UpdateLocationsDto,
+  ) {
+    return this.locationsService.update(+id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.locationsService.remove(+id);
+  }
+}
--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Locations } from './locations.entity';
 import { LocationsController } from './locations.controller';
 import { LocationsService } from './locations.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Locations])],
   controllers: [LocationsController],
-  providers: [LocationsService]
+  providers: [LocationsService],
 })
 export class LocationsModule {}
