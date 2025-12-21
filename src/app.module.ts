98797f8e1f213543a5d2581c81694777de325e9f
andreeesz17 <andryzambrano1709@gmail.com>
2025-12-21 09:02:09 -0500

    App Module completo, para evitar errores de merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PropertiesModule } from './properties/properties.module';
 import { CategoriesModule } from './categories/categories.module';
+import { MailModule } from './mail/mail.module';
+import { LocationsModule } from './locations/locations.module';
+import { ImagesModule } from './images/images.module';
+import { RequestsModule } from './requests/requests.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { PropertyFeaturesModule } from './property-features/property-features.module';
+import { ContractsModule } from './contracts/contracts.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/inmo'),
 
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -22,11 +32,7 @@ import { CategoriesModule } from './categories/categories.module';
       autoLoadEntities: true,
       synchronize: true, 
     }),
-
-    AuthModule,
-    UsersModule,
-    CategoriesModule,
-  ],
+    AuthModule, UsersModule, PropertiesModule, CategoriesModule, MailModule, LocationsModule, ImagesModule, RequestsModule, AppointmentsModule, TransactionsModule, PropertyFeaturesModule, ContractsModule],
   controllers: [AppController],
   providers: [AppService],
 })
