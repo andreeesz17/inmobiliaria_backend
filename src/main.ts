b9d91a013c7f62fd648bbf5a7159513069400de5
Andrés <andryzambrano1709@gmail.com>
2026-01-28 03:17:40 -0500

    main.ts update

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express'; 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
   app.setGlobalPrefix('api');
 
